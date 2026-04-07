74abc0fec81586e30f19c94653a82fe4c6840a57
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2026-04-07 15:59:22 +0200

    migrate(3.x): LLM-guided fixes for Elgg 3.x compatibility

--- start.php
+++ start.php
@@ -6,44 +6,39 @@
  * @author Ismayil Khayredinov <info@hypejunction.com>
  * @copyright Copyright (c) 2016, Ismayil Khayredinov
  */
+
 use Elgg\Notifications\Event;
 use Elgg\Notifications\Notification;
 use hypeJunction\Notifications\MassMail;
+
 require_once __DIR__ . '/autoloader.php';
+
 elgg_register_event_handler('init', 'system', 'notifications_mass_mail_init');
+
 /**
  * Initialize
  * @return void
  */
-function notifications_mass_mail_init()
-{
-    elgg_register_route('mass_mail', ['path' => '/mass_mail/{segments}', 'resource' => 'mass_mail', 'requirements' => ['segments' => '.+'], 'defaults' => ['segments' => '']]);
-    elgg_register_action('mass_mail/send', __DIR__ . '/actions/mass_mail/send.php');
-    elgg_register_plugin_hook_handler('container_permissions_check', 'object', 'notifications_mass_mail_permissions');
-    $subtype = MassMail::SUBTYPE;
-    elgg_register_plugin_hook_handler('get', 'subscriptions', 'notifications_mass_mail_get_subscriptions');
-    elgg_register_notification_event('object', $subtype, ['send']);
-    elgg_register_plugin_hook_handler('prepare', "notification:send:object:{$subtype}", 'notifications_mass_mail_prepare_notification');
-    elgg_register_plugin_hook_handler('register', 'menu:page', 'notifications_mass_mail_page_menu_setup');
+function notifications_mass_mail_init() {
+	elgg_register_route('mass_mail', [
+		'path' => '/mass_mail/{segments}',
+		'resource' => 'mass_mail',
+		'requirements' => ['segments' => '.+'],
+		'defaults' => ['segments' => ''],
+	]);
+
+	elgg_register_action('mass_mail/send', __DIR__ . '/actions/mass_mail/send.php');
+
+	elgg_register_plugin_hook_handler('container_permissions_check', 'object', 'notifications_mass_mail_permissions');
+
+	$subtype = MassMail::SUBTYPE;
+	elgg_register_plugin_hook_handler('get', 'subscriptions', 'notifications_mass_mail_get_subscriptions');
+	elgg_register_notification_event('object', $subtype, ['send']);
+	elgg_register_plugin_hook_handler('prepare', "notification:send:object:{$subtype}", 'notifications_mass_mail_prepare_notification');
+
+	elgg_register_plugin_hook_handler('register', 'menu:page', 'notifications_mass_mail_page_menu_setup');
 }
-/**
- * Mass mail page handler
- * /mass_mail/send/<container_guid>
- *
- * @param array $segments URL segments
- * @return bool
- */
-function notifications_mass_mail_page_handler($segments)
-{
-    $page = array_shift($segments);
-    switch ($page) {
-        case 'send':
-            $container_guid = array_shift($segments);
-            echo elgg_view_resource('mass_mail/send', ['container_guid' => $container_guid]);
-            return true;
-    }
-    return false;
-}
+
 /**
  * Filter container permissions
  *
@@ -51,25 +46,27 @@ function notifications_mass_mail_page_handler($segments)
  * @param string $type   "object"
  * @param bool   $return Permission
  * @param array  $params Hook params
- * @return bool
+ * @return bool|void
  */
-function notifications_mass_mail_permissions($hook, $type, $return, $params)
-{
-    $container = elgg_extract('container', $params);
-    $subtype = elgg_extract('subtype', $params);
-    if ($subtype !== MassMail) {
-        return;
-    }
-    switch ($container->getType()) {
-        case 'object':
-        case 'user':
-            return false;
-        case 'site':
-            return;
-        case 'group':
-            return $container->canEdit();
-    }
+function notifications_mass_mail_permissions($hook, $type, $return, $params) {
+	$container = elgg_extract('container', $params);
+	$subtype = elgg_extract('subtype', $params);
+
+	if ($subtype !== MassMail::SUBTYPE) {
+		return;
+	}
+
+	switch ($container->getType()) {
+		case 'object':
+		case 'user':
+			return false;
+		case 'site':
+			return;
+		case 'group':
+			return $container->canEdit();
+	}
 }
+
 /**
  * Prepare recipients for mass mail
  *
@@ -77,79 +74,127 @@ function notifications_mass_mail_permissions($hook, $type, $return, $params)
  * @param string $type   "subscriptions"
  * @param array  $return Subscriptions
  * @param array  $params Hook params
- * @return array
+ * @return array|void
  */
-function notifications_mass_mail_get_subscriptions($hook, $type, $return, $params)
-{
-    $mass_mail = $params['event']->getObject();
-    if (!elgg_instanceof($mass_mail, 'object', MassMail::SUBTYPE)) {
-        return;
-    }
-    $return = [];
-    // we don't care what other hooks want
-    $container = $mass_mail->getContainerEntity();
-    if ($container instanceof ElggSite) {
-        $recipients = new ElggBatch('elgg_get_entities', ['types' => 'user', 'callback' => false, 'limit' => 0]);
-    } else {
-        $recipients = new ElggBatch('elgg_get_entities', ['types' => 'user', 'relationship' => 'member', 'inverse_relationship' => true, 'relationship_guid' => $container->guid, 'callback' => false, 'limit' => 0]);
-    }
-    $method = $mass_mail->method ?: '_preferred';
-    foreach ($recipients as $recipient) {
-        if ($method == '_preferred') {
-            $methods = (array) get_user_notification_settings($recipient->guid);
-            $return[$recipient->guid] = array_keys($methods);
-        } else {
-            $return[$recipient->guid] = [$method];
-        }
-    }
-    return $return;
+function notifications_mass_mail_get_subscriptions($hook, $type, $return, $params) {
+	$mass_mail = $params['event']->getObject();
+
+	if (!$mass_mail instanceof MassMail) {
+		return;
+	}
+
+	$return = [];
+	// we don't care what other hooks want
+
+	$container = $mass_mail->getContainerEntity();
+	if ($container instanceof \ElggSite) {
+		$recipients = elgg_get_entities([
+			'type' => 'user',
+			'limit' => false,
+			'batch' => true,
+		]);
+	} else {
+		$recipients = elgg_get_entities([
+			'type' => 'user',
+			'relationship' => 'member',
+			'inverse_relationship' => true,
+			'relationship_guid' => $container->guid,
+			'limit' => false,
+			'batch' => true,
+		]);
+	}
+
+	$method = $mass_mail->method ?: '_preferred';
+	foreach ($recipients as $recipient) {
+		if ($method == '_preferred') {
+			$notification_settings = $recipient->getNotificationSettings();
+			$enabled_methods = [];
+			foreach ($notification_settings as $m => $enabled) {
+				if ($enabled) {
+					$enabled_methods[] = $m;
+				}
+			}
+			$return[$recipient->guid] = $enabled_methods;
+		} else {
+			$return[$recipient->guid] = [$method];
+		}
+	}
+
+	return $return;
 }
+
 /**
  * Prepare notification
  *
  * @param string       $hook         "prepare"
- * @param string       $type         "notification:send:object:mass_mail"
+ * @param string       $type         "notification:send:object:notification_mass_mail"
  * @param Notification $notification Notification
  * @param array        $params       Hook params
- * @return Notification
+ * @return Notification|void
  */
-function notifications_mass_mail_prepare_notification($hook, $type, $notification, $params)
-{
-    $event = elgg_extract('event', $params);
-    if (!$event instanceof Event) {
-        return;
-    }
-    $action = $event->getAction();
-    $actor = $event->getActor();
-    $object = $event->getObject();
-    if (!$object instanceof MassMail) {
-        return;
-    }
-    $target = $object->getContainerEntity();
-    $template_params = array('action' => $action, 'actor' => $actor, 'object' => $object, 'target' => $target, 'recipient' => $notification->getRecipient(), 'sender' => $notification->getSender(), 'language' => $notification->language, 'site' => elgg_get_site_entity(), 'params' => $notification->params);
-    $notification->subject = mustache()->render($object->title, $template_params);
-    $notification->summary = $notification->subject;
-    $notification->body = mustache()->render($object->description, $template_params);
-    return $notification;
+function notifications_mass_mail_prepare_notification($hook, $type, $notification, $params) {
+	$event = elgg_extract('event', $params);
+	if (!$event instanceof Event) {
+		return;
+	}
+
+	$action = $event->getAction();
+	$actor = $event->getActor();
+	$object = $event->getObject();
+
+	if (!$object instanceof MassMail) {
+		return;
+	}
+
+	$target = $object->getContainerEntity();
+
+	$template_params = [
+		'action' => $action,
+		'actor' => $actor,
+		'object' => $object,
+		'target' => $target,
+		'recipient' => $notification->getRecipient(),
+		'sender' => $notification->getSender(),
+		'language' => $notification->language,
+		'site' => elgg_get_site_entity(),
+		'params' => $notification->params,
+	];
+
+	$notification->subject = mustache()->render($object->title, $template_params);
+	$notification->summary = $notification->subject;
+	$notification->body = mustache()->render($object->description, $template_params);
+
+	return $notification;
 }
+
 /**
  * Setup page menu
- * 
- * @param string         $hook   "register"
- * @param string         $type   "menu:page"
- * @param ElggMenuItem[] $return Menu
- * @param array          $params Hook params
- * @return ElggMenuItem[]
+ *
+ * @param string          $hook   "register"
+ * @param string          $type   "menu:page"
+ * @param \ElggMenuItem[] $return Menu
+ * @param array           $params Hook params
+ * @return \ElggMenuItem[]
  */
-function notifications_mass_mail_page_menu_setup($hook, $type, $return, $params)
-{
-    if (elgg_in_context('admin')) {
-        $return[] = ElggMenuItem::factory(['name' => 'mass_mail', 'parent_name' => 'administer_utilities', 'section' => 'administer', 'text' => elgg_echo('notifications:mass_mail'), 'href' => "mass_mail/send"]);
-    } else if (elgg_in_context('groups')) {
-        $page_owner = elgg_get_page_owner_entity();
-        if ($page_owner instanceof ElggGroup && elgg_get_plugin_setting('groups_mass_mail', 'notifications_mass_mail')) {
-            $return[] = ElggMenuItem::factory(['name' => 'mass_mail', 'text' => elgg_echo('notifications:mass_mail:groups'), 'href' => "mass_mail/send/{$page_owner->guid}"]);
-        }
-    }
-    return $return;
+function notifications_mass_mail_page_menu_setup($hook, $type, $return, $params) {
+	if (elgg_in_context('admin')) {
+		$return[] = \ElggMenuItem::factory([
+			'name' => 'mass_mail',
+			'parent_name' => 'administer_utilities',
+			'section' => 'administer',
+			'text' => elgg_echo('notifications:mass_mail'),
+			'href' => 'mass_mail/send',
+		]);
+	} else if (elgg_in_context('groups')) {
+		$page_owner = elgg_get_page_owner_entity();
+		if ($page_owner instanceof \ElggGroup && elgg_get_plugin_setting('groups_mass_mail', 'notifications_mass_mail')) {
+			$return[] = \ElggMenuItem::factory([
+				'name' => 'mass_mail',
+				'text' => elgg_echo('notifications:mass_mail:groups'),
+				'href' => "mass_mail/send/{$page_owner->guid}",
+			]);
+		}
+	}
+
+	return $return;
 }
